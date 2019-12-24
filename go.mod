f79cdfb978d92789cddcd5a556cbca099687dac6
Denis S. Runov <denis_runov@prof-p.ru>
2019-12-25 00:32:16 +0300

    Инициализация фтп сервера

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
-module mod
+module github.com/Sterks/FReader
 
 go 1.13
+
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/secsy/goftp v0.0.0-20190720192957-f31499d7c79a
+)
--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"github.com/BurntSushi/toml"
+	"github.com/Sterks/FReader/config"
+	"github.com/Sterks/FReader/services"
+)
 
+func main() {
+	configPath := "config/config.toml"
+	config := config.NewConf()
+	toml.DecodeFile(configPath, &config)
+	ftpreader := services.New(config)
+	ftpreader.Start()
 }
